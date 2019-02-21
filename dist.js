a309283a563a9fba0c5abca387f9a34159de4e24
Jake Coppinger <jake@jakecoppinger.com>
2019-02-21 18:17:39 +1100

    Automated build and commit @ Thu 21 Feb 2019 18:17:39 AEDT

--- dist.js
+++ dist.js
@@ -5865,49 +5865,105 @@ moment.tz.load(require('./data/packed/latest.json'));
 
 },{}],6:[function(require,module,exports){
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var moment = require("moment-timezone");
 var mapboxgl = require("mapbox-gl");
 var timestamp_transforms_1 = require("./timestamp-transforms");
 var mapboxToken = 'pk.eyJ1IjoiamFrZWMiLCJhIjoiY2pkNWF2ZnhqMmZscTJxcGE2amtwZnJ0aiJ9.5OojKRkdmcpPUPiFH1K0_Q';
 Object.getOwnPropertyDescriptor(mapboxgl, "accessToken").set(mapboxToken);
-var host = "https://tiles.cloudspotting.app";
+var tileHost = "https://tiles.cloudspotting.app";
 var mapboxMap = {
     style: 'mapbox://styles/mapbox/streets-v9',
 };
+// Global state - clean this up
 var activeImage = 0;
-/*
-var request = new XMLHttpRequest();
-request.open('GET', 'http://api.ipstack.com/check?access_key=8a00459bf3aa78a9414775b03006c607', true);
-
-request.onload = function() {
-  if (request.status >= 200 && request.status < 400) {
-    // Success!
-    var data = JSON.parse(request.responseText);
-
-    console.log("IP data:", data)
-  } else {
-    // We reached our target server, but it returned an error
-  }
-};
-
-request.onerror = function() {
-  // There was a connection error of some sort
-};
-
-request.send();
-*/
-getAsync(host + "/available-timestamps.json", function (text) {
-    var availableImages = JSON.parse(text);
-    availableImages.sort();
-    var timestamp1 = availableImages[0];
-    var timestamp2 = availableImages[1];
-    var numSets = availableImages.length;
-    var sources = availableImages
+function advanceImage(availableTimestamps, map) {
+    console.log({ availableTimestamps: availableTimestamps });
+    var numSets = availableTimestamps.length;
+    // var zoom = map.getZoom();
+    var maxOpacity = 0.6;
+    var newActiveImage = (activeImage + 1) % numSets;
+    // Using visibility -> none/visible results in new HTTP requests
+    map.setPaintProperty("tileset" + activeImage, 'raster-opacity', 0);
+    map.setPaintProperty("tileset" + newActiveImage, 'raster-opacity', maxOpacity);
+    activeImage = newActiveImage;
+    /*
+    Realtime / Map
+    if 30 mins is 6 seconds
+    5 mins is 1 second
+    1 min -> 0.2 seconds
+    */
+    var secondsPerLoop = 3;
+    var mapSecondsPerMin = secondsPerLoop / 30;
+    var endLoopDelay = 2000;
+    var currentMinute = timestamp_transforms_1.radarTimestampToTime(availableTimestamps[activeImage]);
+    var nextMinute = timestamp_transforms_1.radarTimestampToTime(availableTimestamps[(activeImage + 1) % numSets]);
+    var minsToNextImage = moment.duration(nextMinute.diff(currentMinute)).asMinutes();
+    var newTimeout = nextMinute > currentMinute
+        ? minsToNextImage * mapSecondsPerMin * 1000
+        : endLoopDelay;
+    console.log({ newTimeout: newTimeout, newMinute: currentMinute, futureMinute: nextMinute, minsToNextImage: minsToNextImage });
+    // TODO: is there a way of caching requests for images that haven't changes across timestamps?
+    var imageTime = timestamp_transforms_1.radarTimestampToTime(availableTimestamps[activeImage]);
+    var tz = moment.tz.guess();
+    var prettyTime = imageTime.tz(tz).format('h:mma z');
+    document.getElementById("timestamp").innerHTML = prettyTime;
+    return newTimeout;
+}
+function printZoom() {
+    setTimeout(advanceImage, 300);
+}
+function createLayers(availableTimestamps) {
+    var layers = availableTimestamps.map(function (timestampStr, index) { return ({
+        "id": "tileset" + index,
+        "type": "raster",
+        "source": "tileset" + index,
+        "minzoom": 1,
+        "maxzoom": 22,
+    }); });
+    return layers;
+}
+function createSources(availableTimestamps) {
+    var sources = availableTimestamps
         .map(function (timestampStr) { return ({
         "type": "raster",
         "tiles": [
-            host + "/" + timestampStr + "/{z}/{x}/{y}.png"
+            tileHost + "/" + timestampStr + "/{z}/{x}/{y}.png"
         ],
         "tileSize": 256,
         "scheme": "tms"
@@ -5916,89 +5972,100 @@ getAsync(host + "/available-timestamps.json", function (text) {
         map["tileset" + index] = obj;
         return map;
     }, {});
-    var layers = availableImages.map(function (timestampStr, index) { return ({
-        "id": "tileset" + index,
-        "type": "raster",
-        "source": "tileset" + index,
-        "minzoom": 1,
-        "maxzoom": 22,
-    }); });
-    var map = new mapboxgl.Map({
-        container: 'map',
-        center: [151.2027042, -33.8704156],
-        zoom: 5,
-        style: 'mapbox://styles/mapbox/streets-v9',
-        attributionControl: false
-    });
-    var geolocate = new mapboxgl.GeolocateControl({
-        positionOptions: {
-            enableHighAccuracy: true
-        },
-        fitBoundsOptions: {
-            maxZoom: 10
-        }
-    });
-    map.addControl(geolocate);
-    map.addControl(new mapboxgl.FullscreenControl());
-    map.addControl(new mapboxgl.AttributionControl({
-        customAttribution: "© Bureau of Meteorology"
-    }));
-    map.on('load', function () {
-        document.getElementById("overlay").style.display = 'none';
-        geolocate.trigger();
-        printZoom();
-        for (var key in sourcesDict) {
-            map.addSource(key, sourcesDict[key]);
-        }
-        for (var layer in layers) {
-            map.addLayer(layers[layer]);
-        }
-    });
-    function advanceImage() {
-        var zoom = map.getZoom();
-        var maxOpacity = 0.6;
-        var newActiveImage = (activeImage + 1) % numSets;
-        // Using visibility -> none/visible results in new HTTP requests
-        map.setPaintProperty("tileset" + activeImage, 'raster-opacity', 0);
-        map.setPaintProperty("tileset" + newActiveImage, 'raster-opacity', maxOpacity);
-        activeImage = newActiveImage;
-        /*
-        Realtime / Map
-        if 30 mins is 6 seconds
-        5 mins is 1 second
-        1 min -> 0.2 seconds
-        */
-        var secondsPerLoop = 3;
-        var mapSecondsPerMin = secondsPerLoop / 30;
-        var endLoopDelay = 2000;
-        var currentMinute = timestamp_transforms_1.radarTimestampToTime(availableImages[activeImage]);
-        var nextMinute = timestamp_transforms_1.radarTimestampToTime(availableImages[(activeImage + 1) % numSets]);
-        var minsToNextImage = moment.duration(nextMinute.diff(currentMinute)).asMinutes();
-        var newTimeout = nextMinute > currentMinute
-            ? minsToNextImage * mapSecondsPerMin * 1000
-            : endLoopDelay;
-        console.log({ newTimeout: newTimeout, newMinute: currentMinute, futureMinute: nextMinute, minsToNextImage: minsToNextImage });
-        var imageTime = timestamp_transforms_1.radarTimestampToTime(availableImages[activeImage]);
-        var tz = moment.tz.guess();
-        var prettyTime = imageTime.tz(tz).format('h:mma z');
-        console.log({ prettyTime: prettyTime });
-        document.getElementById("timestamp").innerHTML = prettyTime;
-        setTimeout(advanceImage, newTimeout);
-    }
-    function printZoom() {
-        setTimeout(advanceImage, 300);
-    }
-});
-function getAsync(url, callback) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            callback(this.responseText);
-        }
-    };
-    xhttp.open("GET", url, true);
-    xhttp.send();
+    return sourcesDict;
 }
+function loadMap() {
+    return __awaiter(this, void 0, void 0, function () {
+        var map, geolocate;
+        return __generator(this, function (_a) {
+            map = new mapboxgl.Map({
+                container: 'map',
+                center: [151.2027042, -33.8704156],
+                zoom: 5,
+                style: 'mapbox://styles/mapbox/streets-v9',
+                attributionControl: false
+            });
+            geolocate = new mapboxgl.GeolocateControl({
+                positionOptions: {
+                    enableHighAccuracy: true
+                },
+                fitBoundsOptions: {
+                    maxZoom: 9
+                }
+            });
+            map.addControl(geolocate);
+            map.addControl(new mapboxgl.FullscreenControl());
+            map.addControl(new mapboxgl.AttributionControl({
+                customAttribution: "© Bureau of Meteorology"
+            }));
+            return [2 /*return*/, new Promise(function (resolve, reject) {
+                    map.on('load', function () {
+                        document.getElementById("overlay").style.display = 'none';
+                        geolocate.trigger();
+                        resolve(map);
+                    });
+                })];
+        });
+    });
+}
+function getAsync(url) {
+    return __awaiter(this, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            return [2 /*return*/, new Promise(function (resolve, reject) {
+                    var xhttp = new XMLHttpRequest();
+                    xhttp.onreadystatechange = function () {
+                        if (this.readyState == 4 && this.status == 200) {
+                            resolve(this.responseText);
+                        }
+                        // TODO: Reject somewhere
+                    };
+                    xhttp.open("GET", url, true);
+                    xhttp.send();
+                })];
+        });
+    });
+}
+function main() {
+    return __awaiter(this, void 0, void 0, function () {
+        var mapPromise, timestampsJsonPromise, map, timestampsJson, availableImages, layers, sources, key, layer, delay;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    mapPromise = loadMap();
+                    timestampsJsonPromise = getAsync(tileHost + "/available-timestamps.json");
+                    return [4 /*yield*/, mapPromise];
+                case 1:
+                    map = _a.sent();
+                    return [4 /*yield*/, timestampsJsonPromise];
+                case 2:
+                    timestampsJson = _a.sent();
+                    availableImages = JSON.parse(timestampsJson);
+                    availableImages.sort();
+                    layers = createLayers(availableImages);
+                    sources = createSources(availableImages);
+                    for (key in sources) {
+                        map.addSource(key, sources[key]);
+                    }
+                    for (layer in layers) {
+                        map.addLayer(layers[layer]);
+                    }
+                    _a.label = 3;
+                case 3:
+                    if (!true) return [3 /*break*/, 5];
+                    delay = advanceImage(availableImages, map);
+                    return [4 /*yield*/, sleep(delay)];
+                case 4:
+                    _a.sent();
+                    return [3 /*break*/, 3];
+                case 5: return [2 /*return*/];
+            }
+        });
+    });
+}
+function sleep(ms) {
+    return new Promise(function (resolve) { return setTimeout(resolve, ms); });
+}
+main();
 
 },{"./timestamp-transforms":7,"mapbox-gl":1,"moment-timezone":3}],7:[function(require,module,exports){
 "use strict";
